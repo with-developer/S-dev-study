a321c7ce91ada6c9f6720831e25d2d034f60022c
with-developer <miki3079@naver.com>
2023-07-01 07:20:07 -0400

    update

--- network/main.cpp
+++
@@ -1,9 +0,0 @@
-// main.cpp
-#include <stdio.h>
-#include "sum.h"
-
-int main() {
-  int s = sum(10);
-  printf("result=%d\n", s);
-}
-
--- network/sum.cpp
+++
@@ -1,9 +0,0 @@
-#include "sum.h"
-
-int sum(int n) {
-  int res = 0;
-  for (int i = 1; i <= n; ++i)
-    res += i;
-  return res;
-}
-
