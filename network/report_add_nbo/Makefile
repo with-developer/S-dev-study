f8fad74ff39785f76aa5bdeea0f6cf98e9862588
with-developer <miki3079@naver.com>
2023-07-02 00:45:49 -0400

    add report_add_nbo

---
+++ network/report_add_nbo/Makefile
@@ -0,0 +1,7 @@
+all:add-nbo
+
+add-nbo:main.cpp
+	gcc -o add-nbo main.cpp
+
+clean:
+	rm -f add-nbo
---
+++ network/report_add_nbo/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+
+uint32_t read_file(char* filename){
+	uint32_t number;
+	FILE* file = fopen(filename,"rb");
+
+	if (file == NULL) {
+        	printf("Failed to open the file: %s\n", filename);
+		exit(1);
+    	}
+
+	fread(&number, sizeof(uint32_t), 1, file);
+	fclose(file);
+
+	number = ntohl(number);
+	
+	return number;
+}
+
+
+int main(int argc, char* argv[]) {
+    	if (argc != 3) {
+        	printf("Usage: %s <file1> <file2>\n", argv[0]);
+        	return 1;
+    	}
+    	
+	uint32_t number_a = read_file(argv[1]);
+	uint32_t number_b = read_file(argv[2]);
+	
+	printf("%1$u(0x%1$x) + %2$u(0x%2$x) = %3$u(0x%3$x)\n", number_a, number_b, number_a+number_b);
+}
