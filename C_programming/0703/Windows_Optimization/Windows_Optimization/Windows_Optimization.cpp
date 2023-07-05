167d013fc58cd58af797f197c76beadadf3d984e
이준형 <miki3079@naver.com>
2023-07-05 09:41:33 +0900

    update patch branch

---
+++ C_programming/0703/Windows_Optimization/Windows_Optimization/Windows_Optimization.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void printEvenNumbers(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    printEvenNumbers(numbers, size);
+
+    return 0;
+}
