446d7342d55e70747d602e00c6aceb17e995e22c
이준형 <miki3079@naver.com>
2023-07-10 12:54:50 +0900

    add malloc_student_struct

---
+++ C_programming/0710/func_pointer/func_pointer/func_pointer.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h >
+
+typedef struct lower_upper{
+	int is_lowwer = 0;
+	int is_upper = 0;
+}lower_upper;
+
+struct lower_upper get_lower_upper(char message[100]) {
+	struct lower_upper result;
+
+	for (int i = 0; i <= strlen(message); i++) {
+		if (isupper(message[i])) {
+			result.is_upper++;
+		}
+		else if (islower(message[i])) {
+			result.is_lowwer++;
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	char message[100] = "";
+
+	printf("input text >");
+	scanf("%s", &message);
+
+	struct lower_upper operation;
+	operation = get_lower_upper(message);
+	
+	printf("lower count: %d\nupper count: %d", operation.is_lowwer, operation.is_upper);
+}
---
+++ C_programming/0710/malloc_student/malloc_student/malloc_student.c
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Student {
+    int id;
+    char name[20];
+    double score;
+};
+
+int main()
+{
+    struct Student* students;
+    int student_size=0;
+    printf("몇명의 학생을 추가하시겠습니까? >>");
+    
+    scanf("%d", &student_size);
+
+    students = (struct Student*)malloc(sizeof(struct Student) * student_size);
+    
+    for (int i = 0; i < student_size; i++) {
+        
+        int id = 0;
+        char name[20];
+        double score = 0;
+
+        printf("%d 번째 학생의 id를 입력하세요: ", i + 1);
+        scanf("%d", &id);
+        students[i].id = id;
+
+        printf("%d 번째 학생의 name을 입력하세요: ", i + 1);
+        scanf("%s", name);
+        strcpy(students[i].name, name);
+
+        printf("%d 번째 학생의 score를 입력하세요: ", i + 1);
+        scanf("%lf", &score);
+        students[i].score = score;
+    }
+
+    printf("\n입력된 학생 정보:\n");
+    for (int i = 0; i < student_size; i++) {
+        printf("%d %s %.2f\n", students[i].id, students[i].name, students[i].score);
+        
+    }
+
+    free(students); // 할당된 동적 메모리 해제
+    return 0;
+}
