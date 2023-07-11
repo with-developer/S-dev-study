2f0116322d3bc3aa33e2b78ebb3576d5e1931c6b
이준형 <miki3079@naver.com>
2023-07-11 14:04:15 +0900

    add web_blind_injection_exploit_code

---
+++ C_programming/0710/Win32_GUI_2/Win32_GUI_2/Win32_GUI_2.cpp
@@ -0,0 +1,75 @@
+﻿#include <windows.h>
+
+HINSTANCE hInst;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    hInst = hInstance;
+
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    //wc.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(255, 0, 0));
+    wc.lpszClassName = L"MyWindowClass";
+
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, L"윈도우 클래스 등록 실패", L"오류", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowEx(0, L"MyWindowClass", L"Win32 GUI 애플리케이션", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, L"윈도우 생성 실패", L"오류", MB_OK | MB_ICONERROR);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
+    switch (uMsg)
+    {
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        //FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)CreateSolidBrush(RGB(0, 0, 0)));
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
