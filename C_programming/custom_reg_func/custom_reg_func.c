d5533dc678218c4ef195caf37c3959984d6173c2
이준형 <miki3079@naver.com>
2023-07-26 18:38:29 +0900

    update custom_reg

---
+++ C_programming/custom_reg_func/custom_reg_func.c
@@ -0,0 +1,28 @@
+﻿# define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int reg_func(char* message) {
+	for (int i = 0; i <= strlen(message); i++) {
+		if (message[i] >= 65 && message[i] <= 90) {
+			printf("Find %c\n", message[i]);
+			return 1;
+		}
+	}
+
+	return -1;
+}
+
+int main() {
+	char message[100];
+
+	printf("input message >>");
+	scanf("%99s", message);
+	
+	int result = reg_func(message);
+
+	if (result == 1) printf("exist upper case\n");
+	else printf("not exist upper case\n");
+
+	
+}
