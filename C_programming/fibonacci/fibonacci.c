c1a76cbe4b331390b9f3aa4254ba593e46f300a2
이준형 <miki3079@naver.com>
2023-08-04 12:17:21 +0900

    add fibonacci

---
+++ C_programming/fibonacci/fibonacci.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int fibonacci(int n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n = 20;
+    printf("Fibonacci of %d = %d\n", n, fibonacci(n));
+
+    return 0;
+}
