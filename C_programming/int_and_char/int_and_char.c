cbbd7a63a09bc2dc26316763cdee0baca453079c
이준형 <miki3079@naver.com>
2023-08-04 12:18:03 +0900

    add int_and_char

---
+++ C_programming/int_and_char/int_and_char.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+    int intArr[5] = { 1, 2, 3, 4, 5 };
+    char charArr[6] = "hello";
+
+    // 배열의 크기 출력
+    printf("Size of intArr: %zu bytes\n", sizeof(intArr));
+    printf("Size of charArr: %zu bytes\n", sizeof(charArr));
+
+    // 배열의 내용 출력
+    printf("\nContents of intArr:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", intArr[i]);
+    }
+    printf("\n");
+
+    printf("\nContents of charArr: %s\n", charArr);
+
+    // 각 배열의 첫 번째 원소의 메모리 주소 출력
+    printf("\nAddress of intArr[0]: %p\n", &intArr[0]);
+    printf("Address of charArr[0]: %p\n", &charArr[0]);
+
+    return 0;
+}
